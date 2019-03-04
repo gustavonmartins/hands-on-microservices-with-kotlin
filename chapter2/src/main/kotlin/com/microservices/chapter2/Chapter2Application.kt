e03fc7ae073af44873e1b66162543654b12b18fd
Gustavo Martins <martins.yo@gmail.com>
2019-03-04 22:18:38 +0100

    page 79, but with errata

--- chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -2,7 +2,9 @@ package com.microservices.chapter2
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,7 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
-class Chapter2Application
+class Chapter2Application{
+	@Bean
+	@ConditionalOnExpression("#{'\${service.message.type}'=='simple'}")
+	fun exampleService(): ServiceInterface = ExampleService()
+
+	@Bean
+	@ConditionalOnExpression("#{'\${service.message.type}'=='advance'}")
+	fun advanceService(): ServiceInterface = AdvanceService()
+}
 
 @Controller
 class FirstController{
