9f9ac674b562d9885d13b9e59a0065e808451d61
Gustavo Martins <martins.yo@gmail.com>
2019-03-30 19:22:57 +0100

    pg 84

--- chapter3/src/main/kotlin/com/microservices/chapter3/Chapter3Application.kt
+++ chapter3/src/main/kotlin/com/microservices/chapter3/Chapter3Application.kt
@@ -3,6 +3,7 @@ package com.microservices.chapter3
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class Chapter3Application
 
